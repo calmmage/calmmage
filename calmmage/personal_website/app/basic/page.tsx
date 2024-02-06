231236828da6995afb36cb14f20836e7f978f18e
Petr Lavrov <calmquant@gmail.com>
2024-02-06 04:50:25 +0300

    Add two placeholder sections to the main site

---
+++ calmmage/personal_website/app/basic/page.tsx
@@ -0,0 +1,10 @@
+import { ExternalLink } from '#/ui/external-link';
+
+export default function Page() {
+    return (
+        <div className="prose prose-sm prose-invert max-w-none">
+            <h1 className="text-xl font-bold">Petr Lavrov profile!</h1>
+
+        </div>
+    );
+}
---
+++ calmmage/personal_website/app/showcase/page.tsx
@@ -0,0 +1,10 @@
+import { ExternalLink } from '#/ui/external-link';
+
+export default function Page() {
+    return (
+        <div className="prose prose-sm prose-invert max-w-none">
+            <h1 className="text-xl font-bold">Petr Lavrov Projects Showcase!</h1>
+
+        </div>
+    );
+}
