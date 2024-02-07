b2f465cd734cccaf6d034e1d5284efed5c4a3842
Petr Lavrov <calmquant@gmail.com>
2024-02-07 04:07:38 +0300

    Add first Raycast shell command

---
+++ calmmage/beta/raycast/foam.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Foam
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName foam
+
+# Documentation:
+# @raycast.author petr_lavrov
+# @raycast.authorURL https://raycast.com/petr_lavrov
+
+open -a Visual\ Studio\ Code ~/work/code/seasonal/2024_01_jan/calmmage/calmmage/personal_website/pages/calmmage_knowledge_base/
+
