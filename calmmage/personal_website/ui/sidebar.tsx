a47c0cac50d50bd27828d59340a53d8ed2ad13a0
Petr Lavrov <calmquant@gmail.com>
2024-02-10 03:48:44 +0300

    move sidebar to main content layer

--- calmmage/personal_website/ui/sidebar.tsx
+++ calmmage/personal_website/ui/sidebar.tsx
@@ -17,7 +17,7 @@ export function Sidebar() {
               width: '300px'
           }}
           className={classNames(
-              "fixed top-0 z-10 flex h-screen w-[300px] flex-col border-b border-gray-800 bg-black",
+              "top-0 z-10 flex h-screen w-[300px] flex-col border-b border-gray-800 bg-black",
               "lg:bottom-0 lg:z-auto lg:w-72 lg:border-b-0 lg:border-r lg:border-gray-800"
           )}
       >
