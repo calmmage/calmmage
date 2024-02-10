ba1743aba5f13daf5c8c66aa58b30af5755aa21c
Petr Lavrov <calmquant@gmail.com>
2024-02-10 03:43:50 +0300

    Update root layout to allow full width

--- calmmage/personal_website/app/layout.tsx
+++ calmmage/personal_website/app/layout.tsx
@@ -29,34 +29,18 @@ export default function RootLayout({
                                    }: Readonly<{
   children: React.ReactNode;
 }>) {
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
   return (
-    <html lang="en">
-      <body>
-        {/*<GlobalNav />*/}
-        <Sidebar />
-        {/*{children}*/}
-        <div className="flex justify-center pt-[150px]">
-          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
+      <html lang="en">
+      <body className="flex">
+      <Sidebar/>
 
-            {/*<div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">*/}
-            {/*  <div className="rounded-lg bg-black">*/}
-            {/*    <AddressBar />*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-
-            {children}
-        {/*    <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">*/}
-        {/*      <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>*/}
-        {/*    </div>*/}
-            <Byline className="fixed sm:hidden" />
-          </div>
+      <div className="flex-grow">
+        <div className="mx-auto max-w-full space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
+          {children}
+          <Byline className="fixed sm:hidden"/>
         </div>
+      </div>
       </body>
-    </html>
+      </html>
   );
 }
