26e6be235cd45ac9e9e48ad941905edc5a7d3942
Lev Lavrov <levlevlavrov@gmail.com>
2024-02-11 17:18:46 +0300

    sidebar close/open, fix main page (bottom padding for iframe), clear page and layout

--- calmmage/personal_website/app/page.tsx
+++ calmmage/personal_website/app/page.tsx
@@ -1,16 +1,17 @@
 import { sections } from '@/lib/sections';
-import Image from "next/image";
 import Link from 'next/link';
 
 
 export default function Page() {
   return (
-      <div>
-
-        {/*<div className="space-y-8" style={{ zIndex: 10 }}>*/}
-        <div className="space-y-8" style={{ maxWidth: '1024px', margin: '0 auto' }}>
-          {/*<h1 className="text-xl font-medium text-gray-300">Examples</h1>*/}
-
+      <section>
+        <div
+          className="space-y-8"
+          style={{
+            maxWidth: '1024px',
+            margin: '0 auto',
+            paddingTop: '50px'
+        }}>
           <div className="space-y-10 text-white">
             {sections.map((section) => {
               return (
@@ -45,93 +46,14 @@ export default function Page() {
             })}
           </div>
         </div>
-        <div className="flex z-1 min-h-screen flex-col items-center justify-between p-24">
-          <div style={{padding: '20px'}}>
-            <div style={{position: 'absolute', top: "0%", left: "30%", zIndex: -1}}>
-              <iframe scrolling="no"
-                      src="/particles/index.html"
-                      style={{width: '1000px', height: '1000px', border: 'none', opacity: '100%'}}
-                  // style={{width: '100%', height: '500px', border: 'none'}}
-                      title="Particles Animation"
-              ></iframe>
-            </div>
-          </div>
-          <div
-              className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <a
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                // className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Docs{" "}
-                <span
-                    className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Find in-depth information about Next.js features and API.
-              </p>
-            </a>
 
-            <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Learn{" "}
-                <span
-                    className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Learn about Next.js in an interactive course with&nbsp;quizzes!
-              </p>
-            </a>
-
-            <a
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Templates{" "}
-                <span
-                    className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Explore starter templates for Next.js.
-              </p>
-            </a>
-
-            <a
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Deploy{" "}
-                <span
-                    className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-                Instantly deploy your Next.js site to a shareable URL with Vercel.
-              </p>
-            </a>
-          </div>
+        <div style={{position: 'absolute', top: "0%", left: "30%", zIndex: -1}}>
+          <iframe scrolling="no"
+                  src="/particles/index.html"
+                  style={{width: '1000px', height: '1000px', border: 'none', opacity: '100%'}}
+                  title="Particles Animation"
+          ></iframe>
         </div>
-      </div>
+      </section>
   );
 }
--- calmmage/personal_website/ui/sidebar.tsx
+++ calmmage/personal_website/ui/sidebar.tsx
@@ -1,58 +1,72 @@
 'use client';
 // Sidebar.tsx
 import Link from 'next/link';
-import { MenuAlt2Icon, XIcon } from '@heroicons/react/solid';
-import { sections, type Item } from '@/lib/sections';
+import { sections } from '@/lib/sections';
 import { GlobalNavItem } from '@/ui/global-nav';
 import { useState } from "react";
 import classNames from "classnames";
+import CMIcon from "@/components/Icon";
 
 export function Sidebar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const close = () => setIsOpen(false);
+  const [isOpen, setIsOpen] = useState(true);
+  const onClose = () => setIsOpen(!isOpen);
+
   return (
       <div
           style={{
               height: '100svh',
-              width: '300px'
+              width: '300px',
+              // flex flex-col =>
+              display: 'flex',
+              flexDirection: 'column',
           }}
           className={classNames(
               "top-0 z-10 flex h-screen w-[300px] flex-col border-b border-gray-800 bg-black",
-              "lg:bottom-0 lg:z-auto lg:w-72 lg:border-b-0 lg:border-r lg:border-gray-800"
+              "lg:bottom-0 lg:z-auto lg:w-72 lg:border-b-0 lg:border-r lg:border-gray-800",
           )}
       >
-
-          <div style={{ padding: '20px' }} className="flex h-14 px-4 py-4 lg:h-auto">
-              <Link
-                  href="/"
-                  className="group flex w-full "
-                  onClick={close}
-              >
-                  {/*<div className="h-7 w-7 rounded-full border border-white/30 group-hover:border-white/50">*/}
-                  {/*    /!*<NextLogo />*!/*/}
-                  {/*    <h1> NextLogo should have been here</h1>*/}
-                  {/*</div>*/}
-
+          <div
+              onClick={onClose}
+              style={{ padding: '20px', cursor: 'pointer' }}
+              className="flex h-14 px-4 py-4 lg:h-auto cursor-pointer"
+          >
+              <div className="group flex w-full">
                   <h3 className="font-semibold tracking-wide text-gray-400 group-hover:text-gray-50">
                       Calmmage Home
                   </h3>
-              </Link>
+              </div>
+
+              <CMIcon iconName={isOpen ? 'close' : 'burger'} />
           </div>
+
           <div
               style={{
                   height: 'calc(100svh - 56px)',
                   width: '300px',
                   display: 'flex',
+                  flexDirection: 'column',
                   gap: '40px',
-                  padding: '20px'
+                  padding: '20px',
+                  // это мы делаем анимацию при клике
+                  transform: isOpen ? 'translateX(0)' : 'translateX(-300px)',
+                  // время анимации
+                  transition: '0.8s'
               }}
               className="bg-gray-900 text-white overflow-auto"
           >
               {sections.map((section) => (
-                  <div key={section.name} style={{ gap: '20px' }} className="p-4 flex flex-col">
+                  <div
+                      key={section.name}
+                      style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          gap: '20px'
+                      }}
+                      className="p-4 flex flex-col"
+                  >
                       <div className="font-bold uppercase mb-2">{section.name}</div>
                       {section.items.map((item) => (
-                          <GlobalNavItem key={item.slug} item={item} close={close}/>
+                          <GlobalNavItem key={item.slug} item={item} onClose={onClose} />
                       ))}
                   </div>
               ))}
