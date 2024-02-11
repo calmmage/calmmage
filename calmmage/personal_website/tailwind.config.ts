61908240bf3dd8b65c791add8dab5bc4cec0e303
Reliable Magician <petr.b.lavrov@gmail.com>
2024-02-11 19:55:21 +0300

    Update tailwind.config.ts

--- calmmage/personal_website/tailwind.config.ts
+++ calmmage/personal_website/tailwind.config.ts
@@ -3,9 +3,10 @@ import { Config } from 'tailwindcss';
 
 export default {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,md,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
-    './ui/**/*.{js,ts,jsx,tsx,md,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,md,mdx}",
   ],
   future: {
     hoverOnlyWhenSupported: true,
