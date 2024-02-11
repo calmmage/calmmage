d06c34cd4c25752ecb3b32e727c931166267ef39
Petr Lavrov <calmquant@gmail.com>
2024-02-11 20:15:29 +0300

    Make all animations transparent

--- calmmage/personal_website/public/particles/animation.js
+++ calmmage/personal_website/public/particles/animation.js
@@ -3,6 +3,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 6;
 const renderer = new THREE.WebGLRenderer( { alpha: true } );
+renderer.setClearColor( 0x000000, 0 );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -153,7 +154,6 @@ function updateCubes() {
 const animate = function () {
     updateCubes();
     requestAnimationFrame(animate);
-    renderer.setClearColor( 0x000000, 0 );
     renderer.render(scene, camera);
 };
 
