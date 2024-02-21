bb6d69e7989b65d8bc54593277a6583c3c0929f3
Petr Lavrov <petrlavrov@calmmage.com>
2024-02-21 20:22:14 +0300

    Working abs path file reading in MD.

--- calmmage/beta/raycast_extensions/hello-world-package/src/index.tsx
+++ calmmage/beta/raycast_extensions/hello-world-package/src/index.tsx
@@ -1,8 +1,13 @@
 import { Detail } from "@raycast/api";
-// import { readFileSync } from "fs";
+import { readFileSync } from "fs";
 
 export default function Command() {
   // const markdownContent = readFileSync("resources/sample_markdown.md", "utf-8");
-  const markdownContent = "# Hey! 👋"
+  const path= "/Users/calm/work/code/structured/tools/calmmage/calmmage/beta/raycast_extensions/hello-world-package/src/resources/sample_markdown.md"
+  console.log("path", path)
+  // const markdownContent = "# Hey! 👋"
+  const markdownContent = readFileSync(path, "utf-8");
+  console.log("markdownContent", markdownContent)
+  
   return <Detail markdown={markdownContent} />;
 }
