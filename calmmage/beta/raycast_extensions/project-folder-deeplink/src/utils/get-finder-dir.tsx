70791e364e1c38f137a736ae278c503fbb76c876
Petr Lavrov <petrlavrov@calmmage.com>
2024-02-22 16:45:26 +0300

    Fix get-finder-dir.tsx

--- calmmage/beta/raycast_extensions/project-folder-deeplink/src/utils/get-finder-dir.tsx
+++ calmmage/beta/raycast_extensions/project-folder-deeplink/src/utils/get-finder-dir.tsx
@@ -1,84 +1,43 @@
 import { getSelectedFinderItems, getFrontmostApplication } from "@raycast/api";
-// import {open} from "@raycast/api";
-// import { runAppleScript } from "@raycast/utils";
+import { runAppleScript } from "@raycast/utils";
 import fs from "node:fs/promises";
-// import path from "node:path";
-// import { newTab } from "./uri";
+import { logWithToast } from "./raycast-utils";
 
-// const getSelectedPathFinderItems = async () => {
-//   const script = `
-//     tell application "Path Finder"
-//       set thePaths to {}
-//       repeat with pfItem in (get selection)
-//         set the end of thePaths to POSIX path of pfItem
-//       end repeat
-//       return thePaths
-//     end tell
-//   `;
+const getOpenedFinderDir = async (): Promise<string> => {
+  const app = await getFrontmostApplication();
 
-//   const paths = await runAppleScript(script);
-//   return paths.split(",");
-// };
+  if (app.name !== "Finder") {
+    logWithToast("Finder is not currently the focused app", "No directory selected")
+    return "";
+  }
 
-// const fallback = async (): Promise<boolean> => {
-//   const app = await getFrontmostApplication();
+  const currentDirectory = await runAppleScript(
+    `tell application "Finder" to get POSIX path of (target of front window as alias)`
+  );
 
-//   if (app.name !== "Finder") {
-//     return false;
-//   }
+  if (!currentDirectory) {
+    logWithToast("Could not get current directory from Finder", "No directory selected")
+    return "";
+  }
 
-//   const currentDirectory = await runAppleScript(
-//     `tell application "Finder" to get POSIX path of (target of front window as alias)`
-//   );
-
-//   if (!currentDirectory) {
-//     return false;
-//   }
-
-// //   await open(newTab(currentDirectory));
-
-//   return true;
-// };
+  return currentDirectory;
+};
 
 export const getFinderDir = async () => {
-    console.log("Getting dir from Finder"); // Add console log message here
-//   try {
-    let selectedItems: { path: string }[] = [];
+  console.log("Getting dir from Finder"); // Add console log message here
+  let selectedItems: { path: string }[] = [];
 
-    const app = await getFrontmostApplication();
+  const app = await getFrontmostApplication();
 
-    if (app.name === "Finder") {
-      selectedItems = await getSelectedFinderItems();
-    // } else if (app.name === "Path Finder") {
-    //   const paths = await getSelectedPathFinderItems();
-    //   selectedItems = paths.map((p) => ({ path: p }));
-    }
+  if (app.name === "Finder") {
+    selectedItems = await getSelectedFinderItems();
+  }
 
-    if (selectedItems.length === 0) {
-      // const ranFallback = await fallback();
-
-      // if (ranFallback === false) {
-      //   await showToast({
-      //     style: Toast.Style.Failure,
-      //     title: "No directory selected",
-      //     message: "Please select a directory in Finder or Path Finder first",
-      //   });
-      // }
-
-      return "";
-    }
-    
+  if (selectedItems.length > 0) {
     const results = await Promise.all(selectedItems.map((item) => fs.stat(item.path).then((info) => ({ item, info }))));
-    
-    //    output the resulting path - just the first one for now
+    // output the resulting path - just the first one for now
     return results[0].item.path;
-// } catch (error: any) {
-//     const errorMessage = error.toString() as string;
-//     await showToast({
-//         style: Toast.Style.Failure,
-//         title: "Could not get selected directory",
-//         message: errorMessage,
-//     });
-//     return undefined;
-//   }
+  } else {
+    return await getOpenedFinderDir();
+  }
 }
