a4468ad5804e352b2a33714a0be2811651ec0eeb
Petr Lavrov <petrlavrov@calmmage.com>
2024-03-05 05:33:16 +0300

    [Dev Env] update setup scripts

--- calmmage/dev_env/tools/setup_calmmage_dev_env.sh
+++ calmmage/dev_env/tools/setup_calmmage_dev_env.sh
@@ -1,3 +1,10 @@
+# todo: make this use python or rewwrite it in bash
+
+# step 1: set up dev env using main.py
+root_dir=$(dirname $(realpath $0))
+python $root_dir/../main.py
+
+# step 2: check if required env vars are set
 required_env_vars = [
 "CALMMAGE_PROJECT_TEMPLATES_DIR",
 # "OPENAI_API_KEY",
@@ -19,3 +26,13 @@ for env_var in required_env_vars:
 # export CALMMAGE_PROJECT_TEMPLATES_DIR="/Users/username/Projects/calmage/templates"
 # and then bashrc should have this line:
 # source /path/to/.env
+
+# step 3: install pip packages
+packages = [
+"PyGithub",
+"typer-cli"
+"gitpython",
+]
+
+for package in packages:
+    os.system(f"pip install {package}")
