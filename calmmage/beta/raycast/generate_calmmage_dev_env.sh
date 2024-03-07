ef7c282390e79cd3308169c624d484928535168b
Petr Lavrov <petrlavrov@calmmage.com>
2024-03-08 02:57:38 +0300

    Add new raycast script command - generate_calmmage_dev_env.sh

---
+++ calmmage/beta/raycast/generate_calmmage_dev_env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# - 'Regenerate calmmage dev env
+#     - cd ...
+#     - checkout main
+#     - git pull
+#     - run main.py
+
+cd /Users/calm/work/code/structured/dev/calmmage-dev/calmmage/dev_env
+
+git checkout main
+
+git pull
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/calm/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/Users/calm/miniforge3/etc/profile.d/conda.sh" ]; then
+        . "/Users/calm/miniforge3/etc/profile.d/conda.sh"
+    else
+        export PATH="/Users/calm/miniforge3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+conda activate calmmage
+
+python main.py
+```
