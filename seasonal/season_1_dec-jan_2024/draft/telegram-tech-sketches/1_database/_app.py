ba6d43506a10cd55cf3db25cacf5b3eda368e11d
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-01-17 20:24:26 +0300

    v2025.1.2: telegram tech sketches
    - add ideas
    - add template for the bot
    - add first successful bot example: database usage

---
+++ seasonal/season_1_dec-jan_2024/draft/telegram-tech-sketches/1_database/_app.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+
+class AppConfig(BaseSettings):
+    """Basic app configuration"""
+    telegram_bot_token: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+class App:
+    name = "Telegram Tech Sketches"
+
+    def __init__(self, **kwargs):
+        self.config = AppConfig(**kwargs)
